054da2f14addcbe57a4adc0bdd85a099edfe1779
PDC1\jcoleman <jcoleman1388@gmail.com>
2017-05-24 12:43:01 -0400

    Randomness update
    Worked on randomness JS

--- js/randomness.js
+++ js/randomness.js
@@ -6,13 +6,17 @@
 ***************************************/
 $(document).ready(function() {
 
-    function Pirate (rank, favoriteNumber, id); {
+    function Pirate (rank, favorite, id) {
 
-        this.rank = rank;
-        this.favoriteNumber = favoriteNumber;
-        this.id = id; 
-        this.callFavoriteNumber = function() {
-        	return this.favoriteNumber;
+        this.strRank = rank;
+        this.intFavoriteNumber = favorite;
+        this.strId = id; 
+        
+        this.randomNumber = function() {
+            return (Math.random() * 10) +1;
+        } 
+        this.speak = function () {
+            return "I am the captain and the game has started!";
         }
 
     }
@@ -21,7 +25,12 @@ $(document).ready(function() {
     var firstMatePirate = new Pirate ('First Mate', 8, 'pirate');
     var secondMatePirate = new Pirate ('Second Mate', 9, 'pirate');
     var deckScrubberPirate = new Pirate ('Deck Scrubber', 4, 'pirate');
+    
+    var el = document.getElementById('captain'); 
+    el.innerHTML = captainPirate.speak() + ' my favorite number is ' + captainPirate.intFavoriteNumber;
 
+    var blah = captainPirate.intFavoriteNumber = captainPirate.randomNumber();
+    el.innerHTML += blah;
 
 });
 
