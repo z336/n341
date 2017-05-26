2f72ecaadd17d065c9c340bbdb60484c57a57c26
z336 <jcoleman1388@gmail.com>
2017-05-26 15:36:03 -0400

    Iffy is done
    Iffy is done

--- js/iffy.js
+++ js/iffy.js
@@ -6,12 +6,19 @@
 ***************************************/
 $(document).ready(function() {
 
+    var strQuestion = prompt("Did ye make it to the port on time?", "Yes");
 
-
-
-
-
-
+    if (strQuestion.toUpperCase() === "YES") {
+        document.getElementById("iffy").textContent =
+            "Alrighty! You've earned extra dabloons!";
+    } else {
+        document.getElementById("iffy").textContent = "Walk that plank!";
+    }
 
 });
+
+
+
+
+
 
