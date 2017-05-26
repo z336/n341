35beac203d1c372343b6ac5802c02d37641a4987
z336 <jcoleman1388@gmail.com>
2017-05-26 11:36:58 -0400

    changes
    changes

--- js/randomness.js
+++ js/randomness.js
@@ -6,51 +6,42 @@
 ***************************************/
 $(document).ready(function() {
 
-    function Pirate (rank, favorite, id) {
+    function Pirate (rank, favorite, id) { // Pirate object
 
         this.strRank = rank;
         this.intFavoriteNumber = favorite;
         this.elOutput = document.getElementById(id); 
         
         this.randomNumber = function() {
-            this.intFavoriteNumber = (Math.random() * 10) +1;
-<<<<<<< HEAD
+            this.intFavoriteNumber = Math.floor(Math.random() * 10) +1;
+
         }
         this.speak = function (whatToSay) {
             this.elOutput.innerHTML += whatToSay;
         } 
-    }
-=======
-        } 
-
-        this.speak = function () {
-            this.elOutput.innerHTML += "<br>" + this.intFavoriteNumber;
-        }
 
     };
->>>>>>> origin/master
 
-    var captainPirate = new Pirate ('Captain', 7, 'captain');
+
+    var captainPirate = new Pirate ('Captain', 7, 'captain'); // Instantiate the pirates
     var firstMatePirate = new Pirate ('First Mate', 8, 'pirate1');
     var secondMatePirate = new Pirate ('Second Mate', 9, 'pirate2');
     var deckScrubberPirate = new Pirate ('Deck Scrubber', 4, 'pirate3');
-<<<<<<< HEAD
 
-    captainPirate.speak("Hey, we're starting the game.");
+
+    captainPirate.speak("Hey, we're starting the game. Pick the right number to get me grog!" + "<br>");
     captainPirate.randomNumber();
-    alert(captainPirate.intFavoriteNumber);
-
-    firstMatePirate.randomNumber();
-    firstMatePirate.speak("Here is my rank " + firstMatePirate.strRank + " and I choose" + firstMatePirate.intFavoriteNumber + " .");
-
-=======
->>>>>>> origin/master
     
-    captainPirate.speak();
-    firstMatePirate.speak();
-    secondMatePirate.speak();
-    deckScrubberPirate.speak();
+    firstMatePirate.randomNumber();
+    firstMatePirate.speak("Smith here, the " + firstMatePirate.strRank + ", and I choose " + firstMatePirate.intFavoriteNumber + ".");
 
+    secondMatePirate.randomNumber();
+    secondMatePirate.speak("Jones, " + secondMatePirate.strRank + ", and I choose " + secondMatePirate.intFavoriteNumber + ".");
+
+    deckScrubberPirate.randomNumber();
+    deckScrubberPirate.speak("The bloody " + deckScrubberPirate.strRank + " chooses " + deckScrubberPirate.intFavoriteNumber + ".");
+
+    captainPirate.speak("I chose " + captainPirate.intFavoriteNumber + "!");
+
+    
 });
-
-
