b135c5c7722e7105fbf5d9a5e72b6d014ac355e4
z336 <jcoleman1388@gmail.com>
2017-05-26 21:15:03 -0400

    loops
    loops

--- js/loopingThroughArrays.js
+++ js/loopingThroughArrays.js
@@ -4,13 +4,19 @@
  CREATE DATE: 05/21/2017
  PURPOSE: Loops	
 ***************************************/
+
 $(document).ready(function() {
 
-
-
-
-
-
+    var msg = "";
+	var strCode = prompt("Give me yer code", "L");
+    
+    while (strCode.length > 1) {
+    	msg = alert("too big");
+    	break;
+    }
+    document.getElementById("loop").textContent = "password";
+    
 
 
 });
+
