a14ffe2c259be7b409fd3e99ff92d67f9f05370d
z336 <jcoleman1388@gmail.com>
2017-06-09 16:18:11 -0400

    Triggered
    Triggered

---
+++ js/triggered.js
@@ -0,0 +1,12 @@
+/**************************************
+ TITLE: Triggered						
+ AUTHOR: Jon Coleman			
+ CREATE DATE: 06/09/2017
+ PURPOSE: jQuery events
+***************************************/
+
+$(document).ready(function() {
+
+
+
+});
